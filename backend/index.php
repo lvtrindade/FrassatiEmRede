827515486321b6c9176ffc9736ab31976a2e60a6
lvtrindade <lucasventurintrindade@gmail.com>
2025-04-15 15:06:46 -0300

    conexão php e mysql concluída

--- backend/index.php
+++ backend/index.php
@@ -13,8 +13,8 @@ $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write(json_encode([
         'message' => 'Bem vindo à API Frassati em Rede',
         'routes' => [
-            '/api/test' => 'Rota de Teste'
-        ]
+            '/api/test' => 'Rota de Teste',
+            '/api/db-test' => 'Rota do Teste Banco de Dados']
     ]));
     return $response->withHeader('Content-Type', 'application/json');
 });
@@ -24,6 +24,19 @@ $app->get('/api/test', function (Request $request, Response $response) {
         'success' => true,
         'message' => 'API funcionando!'
     ]));
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/api/db-test', function (Request $request, Response $response) {
+    try {
+        $pdo = new PDO('mysql:host=mysql;dbname=frassati_db', 'root', '');
+        $stmt = $pdo->query('SHOW TABLES');
+        $tables = $stmt->fetchAll(PDO::FETCH_COLUMN);
+        $response->getBody()->write(json_encode([ "sucess" => true, "tables" => $tables ]));
+    } catch (PDOException $e) {
+        $response->getBody()->write(json_encode([ "sucess" => false, "error" => $e->getMessage() ]));
+    }
+
     return $response->withHeader('Content-Type', 'application/json');
 });
 
