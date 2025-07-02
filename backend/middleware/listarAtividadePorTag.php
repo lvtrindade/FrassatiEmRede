a08ffea9fe40a6bc942ab71f57a44292629377a0
lvtrindade <lucasventurintrindade@gmail.com>
2025-07-02 15:42:56 -0300

    clean(archives): limpeza de arquivos.

--- backend/middleware/listarAtividadePorTag.php
+++
@@ -1,5 +0,0 @@
-<?php
-    header("Access-Control-Allow-Origin: *"); 
-    header("Content-Type: application/json; charset=UTF-8"); 
-    header("Access-Control-Allow-Methods: POST, GET, OPTIONS"); 
-    header("Access-Control-Allow-Headers: Content-Type, Authorization");
--- database/frassati_db.sql
+++ database/frassati_db.sql
@@ -1,4 +1,3 @@
--- Criar banco
 CREATE DATABASE IF NOT EXISTS frassati_db;
 USE frassati_db;
 
@@ -9,14 +8,12 @@ CREATE TABLE Usuario (
     role VARCHAR(20) DEFAULT 'ADMIN'
 );
 
--- Tabela de TAGS
 CREATE TABLE Tag (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(30) NOT NULL,
     cor VARCHAR(7) NOT NULL
 );
 
--- Tabela de Atividades (1:N com Tag)
 CREATE TABLE Atividade (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(255) NOT NULL,
@@ -27,7 +24,6 @@ CREATE TABLE Atividade (
     FOREIGN KEY (id_tag) REFERENCES Tag(id) ON DELETE RESTRICT
 );
 
--- Galeria 1:1 com Atividade
 CREATE TABLE Galeria (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_atividade INT UNIQUE,
@@ -35,7 +31,6 @@ CREATE TABLE Galeria (
     FOREIGN KEY (id_atividade) REFERENCES Atividade(id) ON DELETE CASCADE
 );
 
--- Imagens da Galeria (1:N com Galeria)
 CREATE TABLE ImagemGaleria (
     id INT AUTO_INCREMENT PRIMARY KEY,
     id_galeria INT NOT NULL,
@@ -43,7 +38,6 @@ CREATE TABLE ImagemGaleria (
     FOREIGN KEY (id_galeria) REFERENCES Galeria(id) ON DELETE CASCADE
 );
 
--- Tabela de eventos do calendário
 CREATE TABLE Eventos (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     data_evento DATE NOT NULL,
@@ -54,24 +48,21 @@ CREATE TABLE Eventos (
     FOREIGN KEY (id_tag) REFERENCES Tag(id) ON DELETE RESTRICT
 );
 
--- Plano de fundo (background)
 CREATE TABLE Background (
 	id INT AUTO_INCREMENT PRIMARY KEY,
     imagem LONGTEXT NOT NULL
 );
 
--- Índices
 CREATE INDEX idx_data_atividade ON Atividade(data_atividade);
 CREATE INDEX idx_titulo ON Atividade(titulo);
 CREATE INDEX idx_data_calendario ON Eventos(data_evento);
 CREATE INDEX idx_atividade_tag_fk ON Atividade(id_tag);
 
--- Inserção de tags padrão
 INSERT INTO Tag (nome, cor) VALUES
-('Geral', '#A2D5F2'),     -- Azul Pastel
-('Lobinho', '#FFD700'),   -- Amarelo
-('Escoteiro', '#008000'), -- Verde
-('Senior', '#800020'),    -- Grená
-('Pioneiro', '#FF0000');  -- Vermelho
+('Geral', '#A2D5F2'),
+('Lobinho', '#FFD700'),
+('Escoteiro', '#008000'),
+('Senior', '#800020'),
+('Pioneiro', '#FF0000');
 
 INSERT  INTO Usuario (usuario, senha) VALUES ('admin', '$2y$10$mzdCJlE9ltn.HX/ov8ARrOlI5NiKfpZQUXXpbWcG/txGZB3D7BL/2');
