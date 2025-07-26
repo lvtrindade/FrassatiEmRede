3ad92739ba08419ce27c7a3e2854d7e50dcbd3b3
lvtrindade <lucasventurintrindade@gmail.com>
2025-07-25 23:18:53 -0300

    fix(modal evento): Disposição de elementos de forma diferente para cada tipo de evento

--- frontend/src/app/core/modals/modal-evento/modal-evento.component.ts
+++ frontend/src/app/core/modals/modal-evento/modal-evento.component.ts
@@ -6,17 +6,18 @@ import { CommonModule } from '@angular/common';
   selector: 'app-modal-evento',
   imports: [CommonModule],
   templateUrl: './modal-evento.component.html',
-  styleUrl: './modal-evento.component.css'
+  styleUrl: './modal-evento.component.css',
 })
 export class ModalEventoComponent {
   @Input() evento!: Evento;
   @Output() fechar: EventEmitter<void> = new EventEmitter<void>();
 
-  formatarData(data: string): string {
-    return new Date(data).toLocaleDateString('pt-BR');
+  formatarHorario(horario: string): string {
+    return horario?.slice(0, 5);
   }
 
-  formatarHorario(hora: string): string {
-    return hora?.slice(0, 5);
+  formatarData(data: string): string {
+    const [ano, mes, dia] = data.split('-');
+    return `${dia}/${mes}/${ano}`;
   }
 }
