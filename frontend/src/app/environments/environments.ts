def646d1353775676fbac8bb1ad6aeea6668b398
lvtrindade <lucasventurintrindade@gmail.com>
2025-07-28 14:04:47 -0300

    test(connection): Teste com conexão de rede compartilhada.

--- frontend/src/app/environments/environments.ts
+++ frontend/src/app/environments/environments.ts
@@ -1,4 +1,12 @@
+const hostname = window.location.hostname;
+
+let apiHost = 'http://localhost:9000';
+
+if (hostname !== 'localhost' && hostname !== '127.0.0.1') {
+  apiHost = 'http://192.168.2.221:9000';
+}
+
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:9000'
+  apiUrl: apiHost,
 };
